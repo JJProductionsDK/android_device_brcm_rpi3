1ffcc54f5f22e8cb62ece47a20c3b4fa1d143d1b
Konsta <konsta09@gmail.com>
2017-04-11 13:46:46 +0300

    build with su
    * Raspberry Pi 3 doesn't have recovery so user can't install
      su add-on after installing the image. Include su by default
      in builds.
    
    Change-Id: I426b9c3c2de1b2661dc4f6e0633e80a6820c1ded

--- lineage.mk
+++ lineage.mk
@@ -8,6 +8,9 @@ $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 # Inherit device configuration
 $(call inherit-product, device/brcm/rpi3/rpi3.mk)
 
+# Su
+WITH_SU := true
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := rpi3
 PRODUCT_NAME := lineage_rpi3
