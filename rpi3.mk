49caf77a733dba39dcc37df99f4e9d3f3045b94b
Konsta <konsta09@gmail.com>
2017-04-14 08:49:52 +0300

    permissions: improve play store compatibility
    * Most apps need touchscreen permissions - include regular
      tablet_core config instead of stripped rpi3_core config.
    * Many apps require gps permissions for location access.
    
    Change-Id: I1b1cc3e1a06b573fb4c4d8412b39386a8bfc064a

--- rpi3.mk
+++ rpi3.mk
@@ -66,5 +66,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:system/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml
