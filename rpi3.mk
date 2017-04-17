41cb55c9a28341e084b36b1cc18e1818bac45f95
Konsta <konsta09@gmail.com>
2017-04-17 16:34:25 +0300

    audio: add a2dp & r_submix
    Change-Id: I1e99b82fa8d182d55b3534de5fd3afb55f662f2c

--- rpi3.mk
+++ rpi3.mk
@@ -27,13 +27,17 @@ PRODUCT_CHARACTERISTICS := tablet
 
 # Audio
 PRODUCT_PACKAGES += \
+    audio.a2dp.default \
     audio.primary.rpi3 \
+    audio.r_submix.default \
     audio.usb.default
 
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libeffects/data/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:system/etc/a2dp_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:system/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:system/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:system/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:system/etc/usb_audio_policy_configuration.xml
 
 # Bluetooth
