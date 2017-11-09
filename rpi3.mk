377a51ccba898b5da8811dfe4ac50e995a205f4e
Konsta <konsta09@gmail.com>
2017-11-09 19:11:39 +0200

    display: build HIDL HALs and add initial HIDL manifest
    Change-Id: Ie37edef7539cbcd02d4b1158bb913f44a42e1acc

--- rpi3.mk
+++ rpi3.mk
@@ -39,6 +39,7 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service \
     libbt-vendor
 
 # GPS
@@ -46,7 +47,12 @@ PRODUCT_PACKAGES += \
     gps.rpi3
 
 # Graphics
-#PRODUCT_PACKAGES += \
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
+    android.hardware.graphics.mapper@2.0-impl
 #    gralloc.rpi3 \
 #    hwcomposer.rpi3 \
 #    libGLES_mesa
