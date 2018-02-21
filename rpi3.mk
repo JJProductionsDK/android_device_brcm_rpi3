571843c22a768e8a1dff86169b80a2faaba73d19
Konsta <konsta09@gmail.com>
2018-02-21 22:03:01 +0200

    build gps hal
    Change-Id: Idc12618a15375381a2890aca6c012fe2124d6f5b

--- rpi3.mk
+++ rpi3.mk
@@ -34,6 +34,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:system/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:system/etc/usb_audio_policy_configuration.xml
 
+# GPS
+PRODUCT_PACKAGES += \
+    gps.rpi3
+
 # Graphics
 PRODUCT_PACKAGES += \
     gralloc.rpi3 \
