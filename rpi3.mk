25674306d89f66fc9b1da1cbf8d4bae96c787495
Konsta <konsta09@gmail.com>
2018-03-04 11:28:44 +0200

    memtrack: build HIDL HALs and add to manifest

--- rpi3.mk
+++ rpi3.mk
@@ -60,6 +60,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.memtrack@1.0-service \
     memtrack.rpi3
 #    gralloc.rpi3 \
 #    hwcomposer.rpi3 \
