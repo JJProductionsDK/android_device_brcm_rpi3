0b89c3f2e6f843a944f78d8084d817377c66f937
Alex Jonas Jensen <10506550+JJakaJonas@users.noreply.github.com>
2018-10-27 02:41:38 +0200

    Update lineage.mk
    Convert from tablet to TV build

--- lineage.mk
+++ lineage.mk
@@ -4,7 +4,7 @@ TARGET_SCREEN_WIDTH := 1280
 TARGET_SCREEN_HEIGHT := 720
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/cm/config/common_full_tv.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/brcm/rpi3/rpi3.mk)
@@ -14,8 +14,8 @@ WITH_SU := true
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := rpi3
-PRODUCT_NAME := lineage_rpi3
+PRODUCT_NAME := Pi TV
 PRODUCT_BRAND := Raspberry
-PRODUCT_MODEL := Raspberry Pi 3
+PRODUCT_MODEL := Pi 3
 PRODUCT_MANUFACTURER := Raspberry
 PRODUCT_RELEASE_NAME := Raspberry Pi 3
